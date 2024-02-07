77c9dda0ab847c3ccac7107fe1cfc998dda85039
jlm109 <jlm109@alu.ua.es>
2024-02-07 14:42:27 +0100

    Ejercicio 2.D Explicación del supuesto test C5. C6 y C7 añadidos.

--- P00-IntelliJ/src/test/java/ppss/TrianguloTest.java
+++ P00-IntelliJ/src/test/java/ppss/TrianguloTest.java
@@ -66,4 +66,53 @@ public class TrianguloTest {
         assertEquals(resultadoEsperado, resultadoReal);
     }
 
+    /**
+     * Este nuevo test C5 carece de información válida para la prueba del SUT, pues, aunque cambien los valores
+     * con los que se prueba el SUT, el camino independiente que se sigue en el grafo es el mismo y no aporta
+     * valor nuevo al testeo de la unidad.
+    @Test
+    public void C5_tipo_triangulo_should_be_Equilatero_when_three_sides_are_equals() {
+        // ARRANGE
+        int a = 7, b = 7, c = 7;
+        String resultadoEsperado = "Equilatero";
+        // ACT
+        String resultadoReal = tri.tipo_triangulo(a,b,c);
+        // ASSERT
+        assertEquals(resultadoEsperado,resultadoReal);
+    }
+    */
+
+    /**
+     * Con este nuevo test recorremos un nuevo camino independiente en la unidad. Este camino nuevo es
+     * el que resulta de entrar al if que contiene la cadena "Valor a fuera del rango permitido".
+     */
+    @Test
+    public void C6_tipo_triangulo_should_be_a_Fuera_rango_when_a_0() {
+        // ARRANGE
+        int a = -4;
+        int b = 8;
+        int c = 5;
+        String resultadoEsperado = "Valor a fuera del rango permitido";
+        // ACT
+        String resultadoReal = tri.tipo_triangulo(a,b,c);
+        // ASSERT
+        assertEquals(resultadoEsperado,resultadoReal);
+    }
+
+    /**
+     * Con este nuevo test recorremos un nuevo camino independiente en la unidad. Este camino nuevo es
+     * el que resulta de entrar al if que contiene la cadena "Valor b fuera del rango permitido".
+     */
+    @Test
+    public void C7_tipo_triangulo_should_be_b_Fuera_rango_when_b_0() {
+        // ARRANGE
+        int a = 9;
+        int b = 0;
+        int c = 5;
+        String resultadoEsperado = "Valor b fuera del rango permitido";
+        // ACT
+        String resultadoReal = tri.tipo_triangulo(a,b,c);
+        // ASSERT
+        assertEquals(resultadoEsperado,resultadoReal);
+    }
 }
