aac3e1500feec9db3e4f27df6e9fccc37fc4335b
jlm109 <jlm109@alu.ua.es>
2024-05-03 16:41:10 +0200

    Práctica 8 empezada

---
+++ P08-WebDriver/madisonIsland/src/test/java/Ejercicio1/sinPageObject/TestCreateAccount.java
@@ -0,0 +1,12 @@
+package Ejercicio1.sinPageObject;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Tag;
+
+public class TestCreateAccount {
+    @Test
+    @Tag("OnlyOnce")
+    public void S1_scenario_createAccount_should_create_new_account_in_the_demo_store_when_this_account_does_not_exist() {
+
+    }
+}
---
+++ P08-WebDriver/madisonIsland/src/test/java/Ejercicio1/sinPageObject/TestLogin.java
@@ -0,0 +1,15 @@
+package Ejercicio1.sinPageObject;
+
+import org.junit.jupiter.api.Test;
+
+public class TestLogin {
+    @Test
+    public void S2_scenario_loginOK_should_login_with_success_when_user_account_exists(){
+
+    }
+
+    @Test
+    public void S3_scenario_loginFailed_should_fail_when_user_account_not_exists(){
+
+    }
+}
