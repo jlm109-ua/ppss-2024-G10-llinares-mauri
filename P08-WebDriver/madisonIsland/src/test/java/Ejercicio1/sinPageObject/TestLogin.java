4b147f5f107f788abe41b3576e449836a991f236
jlm109 <jlm109@alu.ua.es>
2024-05-08 10:01:08 +0200

    TestLogin sin fallos

--- P08-WebDriver/madisonIsland/src/test/java/Ejercicio1/sinPageObject/TestLogin.java
+++ P08-WebDriver/madisonIsland/src/test/java/Ejercicio1/sinPageObject/TestLogin.java
@@ -1,6 +1,7 @@
 package Ejercicio1.sinPageObject;
 
 import dev.failsafe.internal.util.Assert;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,6 +26,11 @@ public class TestLogin {
         createAccount = new TestCreateAccount();
     }
 
+    @AfterEach
+    void tearDown(){
+        driver.close();
+    }
+
     @Test
     public void S2_scenario_loginOK_should_login_with_success_when_user_account_exists(){
         // 1. Verificamos que el título de la página de inicio es el correcto ("Madison Island").
@@ -44,14 +50,14 @@ public class TestLogin {
         // 4. Rellenamos el campo email con el email de la cuenta que hemos creado en el driver createAccount() y enviamos el formulario.
         String email = "juanjuanitojuan@gmail.com";
         driver.findElement(By.id("email")).sendKeys(email);
-        // driver.findElement(By.id("send2")).click();
+        // driver.findElement(By.id("send2")).click(); NO, REALIZARLO CON SUBMIT()
         driver.findElement(By.id("send2")).submit();
 
         // 5. Verificamos que nos aparece el mensaje "This is a required field" debajo del campo que hemos dejado vacío.
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement required = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("advice-required-entry-pass")));
 
-        Assertions.assertTrue(required.isDisplayed(),"ERROR: Elemento CONFIRMATION no mostrado."); // Confirmamos que se haya mostrado el elemento.
+        Assertions.assertTrue(required.isDisplayed(),"ERROR: Elemento CONFIRMATION de la contraseña no mostrado."); // Confirmamos que se haya mostrado el elemento.
 
         // 6. Rellenamos el campo con la contraseña y volvemos a enviar los datos del formulario.
         String password = "juanito1234";
@@ -67,6 +73,30 @@ public class TestLogin {
 
     @Test
     public void S3_scenario_loginFailed_should_fail_when_user_account_not_exists(){
+        // 1. Verificamos que el título de la página de inicio es el correcto ("Madison Island")
+        String titulo = driver.getTitle();
+        String tituloEsperado = "Madison Island";
+        Assertions.assertEquals(tituloEsperado,titulo);
 
+        // 2. Seleccionamos Account, y a continuación seleccionamos el hiperenlace Login.
+        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a")).click();
+        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+
+        // 3. Verificamos que el título de la página es el correcto ("Customer Login")
+        titulo = driver.getTitle();
+        tituloEsperado = "Customer Login";
+        Assertions.assertEquals(tituloEsperado,titulo);
+
+        // 4. Rellenamos los campos con el email de la cuenta que hemos creado en el driver createAccount() y con un password incorrecto. Enviamos el formulario.
+        String email = "juanjuanitojuan@gmail.com";
+        String password = "estaConstraseñaNoEs";
+        driver.findElement(By.id("email")).sendKeys(email);
+        driver.findElement(By.id("pass")).sendKeys(password);
+        driver.findElement(By.id("send2")).submit();
+
+        // 5. Verificamos que nos aparece el mensaje "Invalid login or password".
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        WebElement invalidPassword = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("body > div > div.page > div.main-container.col1-layout > div > div > div.account-login > ul > li > ul > li > span")));
+        Assertions.assertTrue(invalidPassword.isDisplayed(),"ERROR: Elemento \"Invalid password\" no mostrado.");
     }
 }
