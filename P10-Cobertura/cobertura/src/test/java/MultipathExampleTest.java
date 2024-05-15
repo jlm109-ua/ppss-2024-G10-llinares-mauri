be0bb5fde4e2a89180edbf341e9671bc644932b4
jlm109 <jlm109@alu.ua.es>
2024-05-15 09:49:53 +0200

    Corrección

--- P10-Cobertura/cobertura/src/test/java/MultipathExampleTest.java
+++ P10-Cobertura/cobertura/src/test/java/MultipathExampleTest.java
@@ -2,6 +2,8 @@ import ejercicio1.MultipathExample;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MultipathExampleTest {
     int a, b, c;
     MultipathExample mp;
@@ -11,21 +13,25 @@ public class MultipathExampleTest {
         a = 0;
         b = 0;
         c = 0;
+
+        mp = new MultipathExample();
     }
 
     @Test
     public void test1(){
         a = 1;
         b = 1;
+        c = 0;
 
-        mp.multiPath1(a,b,c);
+        assertEquals(0,mp.multiPath1(a,b,c));
     }
 
     @Test
     public void test2(){
-        a = 5;
-        b = 5;
+        a = 6;
+        b = 6;
+        c = 0;
 
-        mp.multiPath1(a,b,c);
+        assertEquals(12,mp.multiPath1(a,b,c));
     }
 }
